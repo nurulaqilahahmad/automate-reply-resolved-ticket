5773bc1e20054dd75f650d44265d3dfdb148326e
nurulaqilahahmad <nrlaqilahahmd@gmail.com>
2025-04-22 17:31:10 +0800

    Disappear the print dialog

--- mark-resolve.js
+++ mark-resolve.js
@@ -13,17 +13,19 @@ if (trackingID === undefined) {
   const browser = await puppeteer.launch({ headless: false }); // headless set to false to launch full version of browser (browser is displaying)
   const page = await browser.newPage();
 
+  // Open ticket page
   await page.goto(
     "https://www.npra.gov.my/support/admin/admin_ticket.php?track=" + trackingID
   );
   console.log("- Open ticket page.");
 
+  // Login Admin support page
   await page.locator("#regInputUsername").fill("QuestSupport2");
   await page.locator("#regInputPassword").fill("Abc123@");
   await page.locator("#recaptcha-submit").click();
   console.log("- Login.");
 
-  //  KIV
+  //  KIV - Check if the ticket is resolved or not, if not, click resolve, if it's resolved, continue
   // const markAsResolve = await page.locator('div.value a').filter(a => a.innerText === 'Mark as Resolved').waitHandle();
   // if (markAsResolve) {
   //     await markAsResolve.click();
@@ -32,16 +34,31 @@ if (trackingID === undefined) {
   //     console.log('- Ticket is already Resolved.');
   // }
 
-  await page.locator('a[title="Print"]').click();
-  console.log("- Click Print");
+  // Click Print in resolved ticket
+  // await page.locator('a[title="Print"]').click();
+  // console.log("- Click Print");
 
-  window.print();
-  setTimeout("closePrintView()", 3000); // delay required for IE to realise what's going on
-  window.onafterprint = closePrintView();
-  function closePrintView() {
-    // do something
-  }
-  console.log("- Close Print Dialog");
+  // Close Print Dialog
+  // Wait a bit to make sure the print dialog opens
+  // const printUrl = await page.locator('a[title="Print"]').getAttribute('href');
+  // const printPage = await browser.newPage();
+
+  // Override window.print BEFORE navigating to the print page
+  await page.evaluateOnNewDocument(() => {
+    window.print = () => {}; // disable print dialog
+  });
+  await page.goto('https://www.npra.gov.my/support/admin/print.php?track=' + trackingID);
+  console.log("- Go to Print page.");
+
+  const pageContent = await page.locator('body').content();
+  console.log(pageContent);
+  
+  // await new Promise(resolve => setTimeout(resolve, 10000));
+  // console.log("- Wait for 10 seconds.");
+  // Press Escape to close the print dialog
+  // await page.keyboard.press('Escape');
+  // await page.close();
+  // console.log("- Click Escape.");
 
   // await browser.close();
 })();
